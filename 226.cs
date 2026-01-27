54609ad55058c41cad4a2162e5cb1716f7a16988
Luis Fernandes <lprfernandes@gmail.com>
2026-01-27 09:30:54 +0100

    226 - Beats 100%

---
+++ 226.cs
@@ -0,0 +1,66 @@
+/*
+226. Invert Binary Tree
+Easy
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+
+using System.Reflection.Metadata.Ecma335;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        //Base case
+        if (root is null)
+        {
+            return null;
+        }
+        //Recursive case
+        TreeNode tempNode = new();
+        tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
